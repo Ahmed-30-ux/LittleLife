80985dae48143b8307939879574510276dcbf530
Ahmed <amamaadnan3@gmail.com>
2026-07-04 16:56:20 +0500

    fix: add @netlify/blobs dep, fix Netlify function URL mapping

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,4 +2,13 @@ const serverless = require('serverless-http');
 const { createApp } = require('../../server');
 
 const app = createApp();
-exports.handler = serverless(app);
+
+exports.handler = serverless(app, {
+  request: function(request, event, context) {
+    // Netlify strips function base path, leaving /echo instead of /api/echo
+    // Prepend /api so Express routes match
+    if (request.url && !request.url.startsWith('/api')) {
+      request.url = '/api' + request.url;
+    }
+  }
+});
